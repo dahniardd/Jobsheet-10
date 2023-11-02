cf8dc3fe16a7e2ae39edb2b87047e056ac9d092e
dahniardd <dahniardvn09@gmail.com>
2023-11-02 08:44:49 +0700

    modifikasi no10

--- Bioskop06.java
+++ Bioskop06.java
@@ -23,6 +23,12 @@ public class Bioskop06{
         System.out.println(penonton.length);
         for (String[] barisPenonton : penonton){
            System.out.println("Panjang baris :" + barisPenonton.length);
+
+        System.out.println("Penonton pada baris ke-3 : ");
+
+        for (int i = 0; i < penonton[2].length; i++){
+           System.out.println(penonton[2][i]);
     }
+}
 }
 }
